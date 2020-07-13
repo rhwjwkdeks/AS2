296302fccc56df7ee6d94cdd36f94794fd14c6ce
Bryan Nham <bryannham12@gmail.com>
2020-07-12 20:34:42 -0500

    FuelQuoteForm commit with Firebase

--- app/src/main/java/com/example/as2/FuelQuoteForm.java
+++ app/src/main/java/com/example/as2/FuelQuoteForm.java
@@ -1,10 +1,9 @@
 package com.example.as2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -16,32 +15,40 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
-import static com.example.as2.Clientprofile.mypreference;
-import static com.example.as2.Clientprofile.AD1;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FuelQuoteForm extends AppCompatActivity {
-    EditText gallons_value;
-    DatePicker date_value;
-    TextView address_value, sprice_value, tprice_value;
-    SharedPreferences sp;
+    private EditText gallons_value;
+    private DatePicker date_value;
+    private TextView address_value, sprice_value, tprice_value;
 
     private Button submit_button, back_button;
 
-    public static final String mypref = "mypref";
-    public static final String gallons = "gallonsKey";
-    public static final String address = "addressKey";
-    public static final String date = "dateKey";
-    public static final String sprice = "spriceKey";
-    public static final String tprice = "tpriceKey";
+    private final Calendar currentDate = Calendar.getInstance();
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
 
-    final Calendar currentDate = Calendar.getInstance();
+    private final String TAG = "FuelQuoteForm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuel_quote_form);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        final String userID = firebaseAuth.getCurrentUser().getUid();
         gallons_value = (EditText) findViewById(R.id.gallons_value);
         gallons_value.addTextChangedListener(new TextWatcher() {
             @Override
@@ -64,17 +71,16 @@ public class FuelQuoteForm extends AppCompatActivity {
                     double total_price = gal * price;
                     String t_price = "$" + total_price;
                     tprice_value.setText(t_price);
-                    Log.d("Total Price", "Total Price=" + t_price);
+                    Log.d(TAG, "Total Price=" + t_price);
                 }
             }
         });
         initCurrentDate();
-        initDeliveryAddress();
+        initDeliveryAddress(userID);
         sprice_value = (TextView) findViewById(R.id.sprice_value);
         //To be calculated via Pricing Module
         sprice_value.setText("$2.00");
         tprice_value = (TextView) findViewById(R.id.tprice_value);
-        sp = getSharedPreferences(mypref, Context.MODE_PRIVATE);
         submit_button = (Button) findViewById(R.id.submit_button);
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,21 +90,37 @@ public class FuelQuoteForm extends AppCompatActivity {
                     gallons_value.setError("Please enter the number of gallons.");
                     return;
                 }
+                float gvalue = Float.parseFloat(g);
                 String a = address_value.getText().toString().trim();
-                String s = sprice_value.getText().toString();
+                String s = sprice_value.getText().toString().substring(1);
+                float svalue = Float.parseFloat(s);
                 String t = tprice_value.getText().toString();
+                float tvalue = Float.parseFloat(t);
                 int selectedYear = date_value.getYear();
                 int selectedMonth = date_value.getMonth();
                 int selectedDay =  date_value.getDayOfMonth();
                 String prefDate = selectedYear + "-" + selectedMonth + "-" + selectedDay;
 
-                SharedPreferences.Editor editor = sp.edit();
-                editor.putString(gallons, g);
-                editor.putString(address, a);
-                editor.putString(sprice, s);
-                editor.putString(tprice, t);
-                editor.putString(date, prefDate);
-                editor.commit();
+                DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+                Map<String, Object> data = new HashMap<>();
+                data.put("gallonsRequested", gvalue);
+                data.put("date", prefDate);
+                data.put("address", a);
+                data.put("pricePerGallon", svalue);
+                data.put("totalDue", tvalue);
+                documentReference.collection("fuelQuote").add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
             }
         });
         back_button = (Button) findViewById(R.id.back_button);
@@ -110,7 +132,7 @@ public class FuelQuoteForm extends AppCompatActivity {
         });
 
     }
-    public void initCurrentDate() {
+    private void initCurrentDate() {
         date_value = (DatePicker) findViewById(R.id.date_value);
         final int year = currentDate.get(Calendar.YEAR);
         final int month = currentDate.get(Calendar.MONTH);
@@ -126,16 +148,19 @@ public class FuelQuoteForm extends AppCompatActivity {
 
                 if (d > day && y == year && m == month)
                     view.updateDate(year, month, day);
-                Log.d("Date", "Year=" + year + " Month=" + (month + 1) + " day=" + day);
+                Log.d(TAG, "Year=" + year + " Month=" + (month + 1) + " day=" + day);
             }
         });
     }
-    public void initDeliveryAddress() {
+    private void initDeliveryAddress(String userID) {
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+        String address = documentReference.get().getResult().getString("Address1");
+        if(address.isEmpty()) {
+            address = documentReference.get().getResult().getString("Address2");
+        }
         address_value = (TextView) findViewById(R.id.address_value);
-        SharedPreferences sp = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
-        String address = sp.getString(AD1, "");
         address_value.setText(address);
-        Log.d("Address", "Address from Client Profile:" + address);
+        Log.d(TAG, "Address from Client Profile:" + address);
     }
     public void backHome(){
         Intent intent = new Intent(this, Home.class);
